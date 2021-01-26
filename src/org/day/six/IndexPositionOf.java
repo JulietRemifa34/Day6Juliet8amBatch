658bd59727205a1b43008dd62a987179e3d5939f
JulietRemifa34 <ijulietremifa@gmail.com>
2021-01-26 09:48:23 +0530

    New Project

---
+++ src/org/day/six/IndexPositionOf.java
@@ -0,0 +1,12 @@
+package org.day.six;
+
+public class IndexPositionOf {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/org/day/six/LengthOfString.java
@@ -0,0 +1,20 @@
+package org.day.six;
+
+public class LengthOfString {
+
+	public static void main(String[] args) {
+		
+		   String s1= "GreensTechnology";
+		   String s2=" SeleniumAutomationtool";
+		   String s3= "velmurugan";
+		   String s4= "j a v a p r o g r a m";
+		   String s5= "9095484678";
+		   
+		   System.out.printf("The length of the String %s is %s",s1,s1.length());
+		   System.out.printf("\nThe length of the String %s is %s",s2,s2.length());
+		   System.out.printf("\nThe length of the String %s is %s",s3,s3.length());
+		   System.out.printf("\nThe length of the String %s is %s",s4,s4.length());
+		   System.out.printf("\nThe length of the String %s is %s",s5,s5.length());
+		   
+	}
+}
